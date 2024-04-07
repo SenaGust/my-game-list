af3902717a18c8091ef99fd0a44e58c58fa172c3
Gustavo Sena <gustavo.sena@bt.com>
2024-04-07 20:48:44 -0300

    Added cache to request IGDB access token

--- src/env-configs.ts
+++ src/env-configs.ts
@@ -6,8 +6,4 @@ const envConfigSchema = z.object({
   NEXT_PUBLIC_IGDB_SECRET_ID: z.string().min(1),
 });
 
-export const envConfig = envConfigSchema.parse({
-  NEXT_PUBLIC_IGDB_BASE_URL: process.env.NEXT_PUBLIC_IGDB_BASE_URL,
-  NEXT_PUBLIC_IGDB_CLIENT_ID: process.env.NEXT_PUBLIC_IGDB_CLIENT_ID,
-  NEXT_PUBLIC_IGDB_SECRET_ID: process.env.NEXT_PUBLIC_IGDB_SECRET_ID,
-});
+export const envConfig = envConfigSchema.parse({ ...process.env });
--- src/shared/apis/igbd/utils/get-access-token/get-access-token.ts
+++ src/shared/apis/igbd/utils/get-access-token/get-access-token.ts
@@ -2,18 +2,26 @@ import { envConfig } from '@/env-configs';
 import axios from 'axios';
 import { GetAccessTokenResponse } from './get-access-token.types';
 
+const oneHour = 3600;
+
 export async function getAccessToken() {
-  const {
-    data: { access_token, expires_in },
-  } = await axios.post<GetAccessTokenResponse>('https://id.twitch.tv/oauth2/token', undefined, {
-    params: {
-      client_id: envConfig.NEXT_PUBLIC_IGDB_CLIENT_ID,
-      client_secret: envConfig.NEXT_PUBLIC_IGDB_SECRET_ID,
-      grant_type: 'client_credentials',
-    },
-  });
+  const queryParams = {
+    client_id: envConfig.NEXT_PUBLIC_IGDB_CLIENT_ID,
+    client_secret: envConfig.NEXT_PUBLIC_IGDB_SECRET_ID,
+    grant_type: 'client_credentials',
+  };
 
-  console.log({ access_token, expires_in }, 'data');
+  const response = await fetch(
+    'https://id.twitch.tv/oauth2/token?' + new URLSearchParams(queryParams),
+    { method: 'POST', cache: 'force-cache', next: { revalidate: oneHour } }
+  );
 
-  return { accessToken: access_token, expiresIn: new Date(expires_in) };
+  console.log('aaaaa', response);
+
+  const { access_token, expires_in } = await response.json();
+
+  const result = { accessToken: access_token, expiresIn: expires_in / oneHour };
+  console.log(result);
+
+  return result;
 }
